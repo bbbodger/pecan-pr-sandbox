6afeb9bbbf28aa07d9d78ed3087ad65eaff8f5c8
David LeBauer <dlebauer@gmail.com>
2025-07-01 17:33:06 -0400

    update to use pkg::fn syntax and fix NAMESPACE doc

--- modules/data.land/data-raw/create_fertilizer_data.R
+++ modules/data.land/data-raw/create_fertilizer_data.R
@@ -1,26 +1,22 @@
-library(readr)
-library(dplyr)
-library(stringr)
-
 get_swat_fert_table <- function() {
   fertilizer.frt <- "https://raw.githubusercontent.com/swat-model/swatplus/main/data/Ames_sub1/fertilizer.frt"
-  read_table(
+  readr::read_table(
     file = fertilizer.frt,
     skip = 1,
-    col_types = cols(.default = col_character())
+    col_types = readr::cols(.default = readr::col_character())
   ) |>
-    mutate(across(c(min_n, min_p, org_n, org_p, nh3_n), as.numeric)) |>
-    rename(
+    dplyr::mutate(dplyr::across(c(min_n, min_p, org_n, org_p, nh3_n), as.numeric)) |>
+    dplyr::rename(
       fertilizer_name = name,
       fraction_mineral_n = min_n,
       fraction_organic_n = org_n,
       fraction_nh3_n = nh3_n
     ) |>
-    mutate(
-      swat_id = row_number(),
+    dplyr::mutate(
+      swat_id = dplyr::row_number(),
       fraction_no3_n = fraction_organic_n - fraction_nh3_n
     ) |>
-    select(swat_id, fertilizer_name, fraction_mineral_n, fraction_nh3_n, 
+    dplyr::select(swat_id, fertilizer_name, fraction_mineral_n, fraction_nh3_n, 
            fraction_no3_n, fraction_organic_n, description)
 }
 
