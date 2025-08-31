e295507c26ca13212ab42d95e09d7a63c2215dee
Blesson <blessonthomas2005@gmail.com>
2025-09-01 02:11:49 +0530

    sobol

--- base/workflow/R/runModule.run.write.configs.R
+++ base/workflow/R/runModule.run.write.configs.R
@@ -9,7 +9,7 @@
 
 
 runModule.run.write.configs <- function(settings, overwrite = TRUE, input_design = NULL) {
-  
+
   if (PEcAn.settings::is.MultiSettings(settings)) {
     if (overwrite && file.exists(file.path(settings$rundir, "runs.txt"))) {
       PEcAn.logger::logger.warn("Existing runs.txt file will be removed.")
@@ -25,6 +25,7 @@ runModule.run.write.configs <- function(settings, overwrite = TRUE, input_design
     if (is.null(settings$ensemble$samplingspace$parameters$method)) {
       settings$ensemble$samplingspace$parameters$method <- "uniform"
     }
+
       if (is.null(input_design)) {
       ensemble_size <- settings$ensemble$size
       input_design <- PEcAn.uncertainty::generate_joint_ensemble_design( settings = settings, ensemble_size = ensemble_size )
@@ -32,16 +33,57 @@ runModule.run.write.configs <- function(settings, overwrite = TRUE, input_design
     
 
 
+
     #check to see if there are posterior.files tags under pft
     posterior.files <-   settings$pfts %>%
       purrr::map_chr("posterior.files", .default = NA_character_)
-
+      
+    
+    
+    samples.file <- file.path(settings$outdir, "samples.Rdata")
+    if (file.exists(samples.file)) {
+      samples <- new.env()
+      load(samples.file, envir = samples) ## loads ensemble.samples, trait.samples, sa.samples, runs.samples, env.samples
+      trait.samples <- samples$trait.samples
+      
+      
+      trait_sample_indices <- input_design[["param"]]
+      ensemble.samples <- list()
+      for (pft in names(trait.samples)) {
+        pft_traits <- trait.samples[[pft]]
+        ensemble.samples[[pft]] <- as.data.frame(
+          lapply(
+            names(pft_traits),
+            function(trait) pft_traits[[trait]][trait_sample_indices]
+          )
+        )
+        names(ensemble.samples[[pft]]) <- names(pft_traits)
+      }
+      sa.samples <- samples$sa.samples
+      runs.samples <- samples$runs.samples
+      ## env.samples <- samples$env.samples
+      
+      all.param.samples<- list(
+        trait.samples = trait.samples,
+        ensemble.samples = ensemble.samples,
+        sa.samples = sa.samples,
+        runs.samples = runs.samples
+        # env.samples = samples$env.samples  # Uncomment if needed
+      )
+      
+      
+    } else {
+      PEcAn.logger::logger.error(samples.file, "not found, this file is required by the run.write.configs function")
+    }
+    
+    
     return(PEcAn.workflow::run.write.configs(
       settings = settings,
       write = isTRUE(settings$database$bety$write), # treat null as FALSE
       posterior.files = posterior.files,
       overwrite = overwrite,
-      input_design = input_design
+      input_design = input_design,
+      all.param.samples = all.param.samples
     ))
   } else {
     stop("runModule.run.write.configs only works with Settings or MultiSettings")
