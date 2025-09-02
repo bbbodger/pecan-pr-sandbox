b09579d702942f426de4fe84db237644d4f5cfdb
Blesson Thomas <bthomas3@scc-wi3.scc.bu.edu>
2025-09-02 17:24:53 -0400

    update

--- modules/uncertainty/sobol/sobol.R
+++ modules/uncertainty/sobol/sobol.R
@@ -16,133 +16,19 @@ library(PEcAn.SIPNET)
 
 #reading XML 
 
-args <- list(continue = FALSE)
-
-settings <- PEcAn.settings::read.settings("/projectnb/dietzelab/bthomas/pecan_runs/sipnet_test/pecan_updated.xml")
-settings<-settings[1]
-
-
-
-
+settings <- PEcAn.settings::read.settings(xmlpath)
 
 #conducting the sampling 
 
+input_design <- PEcAn.uncertainty::generate_joint_ensemble_design(settings=settings,ensemble_size=ensemble_size, sobol=TRUE)
 
-
-ensemble_size <- 4
-Xinput_design <- PEcAn.uncertainty::generate_joint_ensemble_design(settings=settings,ensemble_size=ensemble_size, sobol=TRUE)
-input_design <- Xinput_design$input_design
-all.param.samples <- Xinput_design$all.param.samples
-sobol_obj <- Xinput_design$sobol_obj 
-
-
-#convverting input indices to input
-
-
-
-# Sample inputs 
-#ipsamples <- list()
-
-#input_tags <- names(settings$run$inputs)
-#input_tags
-#for (input_tag in input_tags) {
-#  if (input_tag %in% colnames(input_design)) {
-#    input_paths <- settings$run$inputs[[input_tag]]$path  # List of all possible paths
-#    if (!is.list(input_paths) || length(input_paths) < max(input_design[[input_tag]])) {
-#      stop(paste("Not enough paths for", input_tag, "- max index:", max(input_design[[input_tag]]), "but only", length(input_paths), "available"))
-#    }
-#    input_indices <- input_design[[input_tag]]
-#    ipsamples[[input_tag]] <- list(
-#      ipsamples = lapply(input_indices, function(idx) input_paths[[idx]])  # Select paths by index
-#    )
-#  } else {
-#    message(paste("No column for", input_tag, "in input_design - skipping sampling"))
-#  }
-#}
-#input file path to inputs
-#
-#
-
-
-# extracting parameter 
-
-
-#samples.file <- file.path(settings$outdir, "samples.Rdata")
-#if (file.exists(samples.file)) {
-#  samples <- new.env()
-#  load(samples.file, envir = samples) ## loads ensemble.samples, trait.samples, sa.samples, runs.samples, env.samples
-#  trait.samples <- samples$trait.samples
- 
-#  trait_sample_indices <- input_design[["param"]]
-#  ensemble.samples <- list()
-#  for (pft in names(trait.samples)) {
-#    pft_traits <- trait.samples[[pft]]
-#    ensemble.samples[[pft]] <- as.data.frame(
-#      lapply(
-#        names(pft_traits),
-#        function(trait) pft_traits[[trait]][trait_sample_indices]
-#      )
-#    )
-#    names(ensemble.samples[[pft]]) <- names(pft_traits)
-#  }
-#  sa.samples <- samples$sa.samples
-#  runs.samples <- samples$runs.samples
-  ## env.samples <- samples$env.samples
-  
-#} else {
-#  PEcAn.logger::logger.error(samples.file, "not found, this file is required by the run.write.configs function")
-#}
-
-#ensemble.samples
-#all_params <- ensemble.samples$temperate.deciduous.HPDA
-
-
-
-#creating sobol object
-
-
-
-
-
-#param_count <- ncol(all_params)  # Number of parameters (e.g., 13)
-#param_count
-#N_small <- ceiling(50 / (param_count + 2))  # ~2 for 50 rows; adjust as needed
-#X1_small <- all_params[1:(N_small), ]  # First N rows
-#X2_small <- all_params[(N_small + 1):(2 * N_small), ]  # Next N rows
-#sobol_obj <- soboljansen(model = NULL, X1 = X1_small, X2 = X2_small)
-#U <- sobol_obj$X
-#length(U)
-
-
-
-
-#X1 <- all_params[1:25, ]
-#X2 <- all_params[26:50, ]
-#sobol_obj <- soboljansen(model = NULL, X1 = X1, X2 = X2)
-#U <- sobol_obj$X
-#ensemble.samples$temperate.deciduous.HPDA <-U
-#all.param.samples <- list(
-#  trait.samples = trait.samples,
-#  ensemble.samples = ensemble.samples ,
-#  sa.samples = sa.samples,
-#  runs.samples = runs.samples
-  # env.samples = samples$env.samples  # Uncomment if needed
-#)
-
-
-#running the site configs 
-
-#check to see if there are posterior.files tags under pft
-posterior.files <-   settings$pfts %>%
-  purrr::map_chr("posterior.files", .default = NA_character_)
 
 PEcAn.workflow::run.write.configs(
                                   settings = settings,
                                   write = isTRUE(settings$database$bety$write), # treat null as FALSE
                                   posterior.files = posterior.files,
                                   overwrite = TRUE ,
-                                  input_design = input_design,
-                                  all.param.samples = all.param.samples 
+                                  input_design = input_design
                                  )
 
 
@@ -172,15 +58,6 @@ for (i in run_ids) {
                            
   all_model_out[[i]] <- model_out
 }
-
-
-
-
-
-
-
-
-
 
 #conducting the sobol
 
